a6e5431ffe86a519d2dd02a2da15d883802b04e2
Matheus Matttesco <matheus.mattesco@edu.unifil.br>
2023-05-16 12:07:55 -0300

    Alterações do ultimo Commit

--- mab.py
+++ mab.py
@@ -19,6 +19,9 @@ class Arm:
             return round(random.uniform(0, self.maxReward), 4)
         else:
             return round(random.uniform(self.fakeRewards[arm][0], self.fakeRewards[arm][1]), 4)
+    
+    def get_reward(self, arm):
+        return round(random.uniform(self.fakeRewards[arm][0], self.fakeRewards[arm][1]), 4)
 
 class MabAdversarial:
     def __init__(self, numArms, maxReward, gamma, T, adversarial_prob):
@@ -29,14 +32,18 @@ class MabAdversarial:
         self.weights = [1.0] * self.arms.numArms
         self.rewards = [1.0] * self.arms.numArms
         self.arm_counts = [0] * self.arms.numArms
+        self.arm_counts2 = [0] * self.arms.numArms
         self.rewards_history = [[] for _ in range(self.arms.numArms)]
         self.fakeRewards = self.arms.fakeRewards
         self.choicesArms = []
         self.allRewards = []
+        self.allRewards2 = []
         self.regret = []
+        self.regret2 = []
         self.mean_rewards = []
         self.adversarial_prob = adversarial_prob
         self.results = []
+        self.results2= []
 
     def select_arm(self, t):
         exploration_prob = max(0, self.gamma * t / self.T) 
@@ -62,7 +69,7 @@ class MabAdversarial:
         weights_means = [weight / sum(self.weights) for weight in self.weights]
         
         return self.mean_rewards, self.arm_counts, self.allRewards, self.regret, self.fakeRewards, self.results
-        
+    
     def run_no_adversarial(self):
         for t in range (self.T):
             arm = self.select_arm(t)
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import time
 import pandas as pd
 import plotly.graph_objects as go
 
+
 st.set_page_config("MAB-Adversarial", "🐙", layout="wide")
 image = Image.open('img/mab.png')
 logo = Image.open('img/logo-unifil.png')
 col1, col2, col3 = st.columns([6, 18, 6])
-
 
 
 texto_apresentacao = "  O código implementa o problema do Multi-Armed Bandit Adversarial em um ambiente simulado. O usuário pode definir o número de braços e o número de interações para o problema. O objetivo do agente é escolher o braço que maximiza a recompensa ao longo do tempo. Cada braço tem uma recompensa associada desconhecida. O algoritmo utilizado é o exp3, que leva em conta tanto a recompensa obtida quanto a exploração de braços menos conhecidos. A cada interação, o algoritmo atualiza as estimativas das recompensas e os pesos dos braços. O código exibe gráficos da evolução das recompensas médias, do número de vezes que cada braço foi usado e do arrependimento acumulado ao longo do tempo."
@@ -38,7 +38,7 @@ with co2:
     button2 = st.button('Saber mais 🔎', key='button2') 
     if button2:
         st.write("Representa o valor máximo de recompensa que o agente pode receber ao puxar uma determinada alavanca. Em outras palavras, é o valor máximo que o agente pode receber por escolher a melhor opção.")
-    T = st.number_input("Número de Interações", value=100, min_value=1, max_value=10000)
+    T = st.number_input("Número de Interações", value=500, min_value=1, max_value=10000)
     button3 = st.button('Saber mais 🔎', key='button3')
     if button3:
         st.write("Representa o número de vezes que o agente pode puxar as alavancas. Quanto maior o número de interações, maior a oportunidade para o agente aprender e aprimorar sua estratégia de escolha.")
@@ -47,7 +47,7 @@ with co4:
     button4 = st.button('Saber mais 🔎', key='button4')
     if button4:
         st.write("Representa a proporção entre a escolha de uma alavanca com base no valor que ela oferece (exploração) e a escolha de uma alavanca com base em sua frequência de escolha até o momento (explotação). Uma taxa de exploração alta significa que o agente está disposto a experimentar opções diferentes, enquanto uma taxa de exploração baixa significa que ele está se concentrando nas opções que lhe deram as melhores recompensas até o momento.")    
-    adversarial_prob=A = st.slider("Chance de Haver Adversários", min_value=0.0, max_value=1.0, value=0.10, step=0.01)
+    adversarial_prob=A = st.slider("Chance de Haver Adversários", min_value=0.0, max_value=1.0, value=0.20, step=0.01)
     button5 = st.button('Saber mais 🔎', key='button5')
     if button5:
         st.write("Representa a possibilidade de que o próprio ambiente do MAB possa ser enganoso ou instável, fornecendo feedbacks incorretos ou variáveis")
@@ -65,7 +65,6 @@ with co3:
                 mean_rewards, arm_counts, allRewards, regret, fakeRewards, results = mab.run()
                 mean_rewards2, arm_counts2, allRewards2, regret2, results2 = mab.run_no_adversarial()
             st.success('Concluído!')
-            
             rewards1, rewards2 = zip(*fakeRewards)
             data = {'Braço': [f'Braço {i+1}' for i in range(len(arm_counts))],
                     'Seleções': arm_counts,
@@ -82,6 +81,7 @@ with co3:
                     'Recompensas Max Esperada': rewards2}
             df2 = pd.DataFrame(data)
             st.write(df2)
+            
 
 
             total_selecoes = sum(arm_counts)
@@ -97,6 +97,7 @@ with co3:
             fig = go.Figure(data=[go.Pie(labels=labels, values=porcentagens, hole=0.3)])
             fig.update_layout(title='Porcentagem de seleções por braço sem Adversarial')
             st.plotly_chart(fig)
+
 
             df = pd.DataFrame(results, columns=['Interação', 'Braço escolhido', 'Recompensa'])
             recompensas_braços = {}
@@ -121,7 +122,6 @@ with co3:
                 fig.add_trace(go.Scatter(x=recompensas.index, y=recompensas, mode='lines', name=f'Braço {braço+1}'))
             fig.update_layout(title='Grafico de Recompensas sem o Adversarial', xaxis_title='Interação', yaxis_title='Recompensa')
             st.plotly_chart(fig)
-
 
 
 
